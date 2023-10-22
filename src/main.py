6ffd7565dd8276b7feaa46dd9cbc7b7962bb4670
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-10-22 03:23:55 +0000

    updae

--- src/main.py
+++ src/main.py
@@ -1,35 +1,44 @@
+"""
+ETL-Query script
+"""
 import sqlite3
-import unittest
+from prettytable import PrettyTable
 
-class TestDatabaseQuery(unittest.TestCase):
-    def setUp(self):
-        self.db_path = "src/data/WorldSmallDB.db"
 
-    def test_query_random_records(self):
-        conn = sqlite3.connect(self.db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM WorldSmallDB ORDER BY RANDOM() LIMIT 5")
-        data = cursor.fetchall()
-        conn.close()
-        self.assertEqual(len(data), 5)
+def query():
+    db_path = "src/data/WorldSmallDB.db"
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
 
-    def test_query_records_per_continent(self):
-        conn = sqlite3.connect(self.db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT region, COUNT(*) AS N FROM WorldSmallDB GROUP BY region")
-        data = cursor.fetchall()
-        conn.close()
-        self.assertTrue(len(data) > 0)
+    print("\nLet's quickly review our database. Let's take a sample of how it is constructed.\n")
+    cursor.execute("SELECT * FROM WorldSmallDB ORDER BY RANDOM() LIMIT 5")
+    print_table(cursor, cursor.fetchall())
 
-    def test_query_gdp_per_continent(self):
-        conn = sqlite3.connect(self.db_path)
-        cursor = conn.cursor()
-        cursor.execute(
-            "SELECT region, AVG(gdppcap08), MIN(gdppcap08), MAX(gdppcap08) FROM WorldSmallDB GROUP BY region"
-        )
-        data = cursor.fetchall()
-        conn.close()
-        self.assertTrue(len(data) > 0)
+    print("\nHow many records per continent does our database have?\n")
+    cursor.execute(
+        "SELECT region, COUNT(*) AS N FROM WorldSmallDB GROUP BY region"
+    )
+    print_table(cursor, cursor.fetchall())
+
+    print("\nHow does Gross Domestic Product per capita behave in 2008 in each continent? What are its mean, maximum, and minimum values?\n")
+    cursor.execute(
+        "SELECT region, AVG(gdppcap08), MIN(gdppcap08), MAX(gdppcap08) FROM WorldSmallDB GROUP BY region"
+    )
+    print_table(cursor, cursor.fetchall())
+    conn.close()
+
+def print_table(cursor, data):
+    table = PrettyTable()
+    table.field_names = [i[0] for i in cursor.description]
+    for row in data:
+        table.add_row(row)
+    print(table)
+
+def main():
+
+    # Query
+    print("Consultando datos...")
+    query()
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
